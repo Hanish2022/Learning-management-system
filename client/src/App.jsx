df3b6234a141cc22058b0387d2b64d691a3cc963
Hanish2022 <itz.hanish@gmail.com>
2024-11-28 18:09:27 +0530

    courses page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HeroSection from './pages/student/HeroSection'
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import { RouterProvider } from 'react-router'
+import Courses from './pages/student/Courses'
 
 
 const appRouter = createBrowserRouter([
@@ -16,7 +17,7 @@ const appRouter = createBrowserRouter([
         path: "/",
         element:( <>
           <HeroSection />
-          {/* courses */}
+          <Courses/>
         </>
         ),
       },
