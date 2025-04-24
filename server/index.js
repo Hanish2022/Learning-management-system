f480c82aaf846e1e1e50838887951867da1e5db5
Hanish2022 <itz.hanish@gmail.com>
2025-04-25 02:38:08 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173" || "http://localhost:5174",
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://learning-management-system-1-12yu.onrender.com"],
     credentials: true,
   })
 );
